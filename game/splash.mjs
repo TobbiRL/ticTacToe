829bfd22a1c3dc281e1e80955f3d4694755eab0f
TobbiRl <torbjornrl@uia.no>
2024-10-03 19:27:16 +0200

    added color to splash using ansi.color + $ansi.reset to lessen writing

--- game/splash.mjs
+++ game/splash.mjs
@@ -1,3 +1,4 @@
+import { ANSI } from "./ansi.mjs";
 
 const ART = `
  ______  ____     ___      ______   ____       __      ______   ___      ___
@@ -12,17 +13,16 @@ const ART = `
 
 
 const ART2 = `
- ______  ____     ___    ______    _____        __       ______    ___     _____
-|      ||    |   /  _]  |      |  /     \\      /  ]     |      | /    \\   |    _|    
-|      | |  |   /  /    |      | |   o   |    /  /      |      ||     |   |   |
-|_|  |_| |  |  /  /     |_|  |_| |   _   |   |  /       |_|  |_||  O  |   |   |__   
-  |  |   |  | |  \\        |  |   |  | |  |   |  \\         |  |  |     |   |   ___|    
-  |  |   |  |  \\  \\__     |  |   |  | |  |   \\   \\__      |  |  |     |   |   |__
-  |__|  |____|  \\_____]   |__|   |__| |__|    \\______|    |__|   \\___/    |______|
-  
+${ANSI.COLOR.GREEN} ______  ____     ___    ${ANSI.COLOR.BLUE}______    _____        __       ${ANSI.COLOR.RED}______    ___     _____
+${ANSI.COLOR.GREEN}|      ||    |   /  _]  ${ANSI.COLOR.BLUE}|      |  /     \\      /  ]     ${ANSI.COLOR.RED}|      | /    \\   |    _|    
+${ANSI.COLOR.GREEN}|      | |  |   /  /    ${ANSI.COLOR.BLUE}|      | |   o   |    /  /      ${ANSI.COLOR.RED}|      ||     |   |   |
+${ANSI.COLOR.GREEN}|_|  |_| |  |  /  /     ${ANSI.COLOR.BLUE}|_|  |_| |   _   |   |  /       ${ANSI.COLOR.RED}|_|  |_||  O  |   |   |__   
+${ANSI.COLOR.GREEN}  |  |   |  | |  \\        ${ANSI.COLOR.BLUE}|  |   |  | |  |   |  \\         ${ANSI.COLOR.RED}|  |  |     |   |   ___|    
+${ANSI.COLOR.GREEN}  |  |   |  |  \\  \\__     ${ANSI.COLOR.BLUE}|  |   |  | |  |   \\   \\__      ${ANSI.COLOR.RED}|  |  |     |   |   |__
+${ANSI.COLOR.GREEN}  |__|  |____|  \\_____]   ${ANSI.COLOR.BLUE}|__|   |__| |__|    \\______|    ${ANSI.COLOR.RED}|__|   \\___/    |______|
+${ANSI.RESET}
   `
-
-
+ 
   
 function showSplashScreen() {
     console.log(ART2);
